2c85f338d3a7a89a0d35c10add7dc596fed90629
Dyamez <dyamez@outlook.com>
2023-07-24 14:47:18 -0700

    comics-in-context

---
+++ src/App.jsx
@@ -0,0 +1,36 @@
+import logo from './logo.svg';
+import './App.css';
+import React from 'react';
+import './App.css';
+
+import UserInterface from "./UserInterface"
+
+function App() {
+  const stylin = {
+    background: "#1ECD97",
+    display: "inline-block",
+    width: 120,
+    height: 55,
+    fontSize: 18,
+    letterSpacing: 1,
+    border: "2px solid #1ECD97",
+    borderRadius: 15
+  }
+
+  return (
+    <main>
+     <h1
+          style={{
+            color: "#FEE001",
+            fontFamily: "Luminari, fantasy",
+            fontSize: 50
+          }}
+        >
+          Welcome to Comics Galore!
+        </h1>
+        <UserInterface />
+    </main>
+  );
+}
+
+export default App;
